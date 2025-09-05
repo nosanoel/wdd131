18d0213d942ec55e171a80004bd7fd233eabcd90
unknown <idehennosa231@gmail.com>
2025-09-05 04:43:20 +0100

    Initial commit

---
+++ scripts/getdatas.js
@@ -0,0 +1,3 @@
+// getdates.js
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
